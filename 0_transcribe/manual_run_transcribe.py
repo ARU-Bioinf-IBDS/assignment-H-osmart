34e4144206b15acba8f415daf5e2b5fb006b1dd9
Oliver Smart <oliver.s.smart@gmail.com>
2018-10-17 18:36:02 +0100

    alter to take input sequence from the command line rather than prompt for it.

--- 0_transcribe/manual_run_transcribe.py
+++ 0_transcribe/manual_run_transcribe.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 """
-Transcribing DNA into RNA
-from http://rosalind.info/problems/rna/
+Script to allow manual testing of the transcribe_dna_to_rna
+function.
 """
+import argparse
 from transcribe import transcribe_dna_to_rna
+
+
+def get_dna_sequence_from_command_line():
+    """ gets the input DNA sequence from the command line """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('INPUT_DNA_SEQUENCE',
+                        help='the DNA sequence to test (must be specified)')
+    args = parser.parse_args()
+    return args.INPUT_DNA_SEQUENCE
 
 
 def main():
     """ main function allows you to manually test """
-    dna = input('Enter DNA sequence> ')
+    dna = get_dna_sequence_from_command_line()
     rna = transcribe_dna_to_rna(dna)
-    print(' RNA transcription: ' + rna)
+    print(rna)
 
 # run the main method if python is run as script
 if __name__ == "__main__":
