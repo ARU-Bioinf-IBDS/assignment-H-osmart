d3be35f42a91d2ef3cac7b204a2b2b8a492015a2
Oliver Smart <oliver.s.smart@gmail.com>
2018-12-27 15:03:45 +0000

    stub implementation of fasta_highest_gc

---
+++ 2_fasta_highest_gc/fasta_highest_gc.py
@@ -0,0 +1,7 @@
+"""
+reads a fasta file containing DNA sequences and reports which
+entry has the highest percentage GC content.
+
+written to solve Rosalind Problem "Computing GC Content"
+http://rosalind.info/problems/gc/
+"""
