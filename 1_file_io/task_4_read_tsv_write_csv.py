5b4f899dd6a0db4bf298231656b41c0c48964ab2
Oliver Smart <oliver.s.smart@gmail.com>
2018-12-30 17:04:53 +0000

    task4 TSV read, CSV write

---
+++ 1_file_io/task_4_read_tsv_write_csv.py
@@ -0,0 +1,7 @@
+# instructions: write Python to read in dat from tsv file 
+# "lmb_nobel_prizes.tsv" and 
+# (a) print out data read.
+# (b) save the dates and names in a CSV file "lmb_out.tsv"
+#     replacing the "&" character with "and".
+# Question: in 2002 there are 3 prize winners so there is a comma in the names field
+# how is this handled in the output CSV file?
