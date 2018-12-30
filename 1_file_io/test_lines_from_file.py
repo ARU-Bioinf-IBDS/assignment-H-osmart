d263c3367fdf0600f6a50544efb68ea3c04edc02
Oliver Smart <oliver.s.smart@gmail.com>
2018-12-30 14:18:11 +0000

    add comment explaining test

--- 1_file_io/test_lines_from_file.py
+++ 1_file_io/test_lines_from_file.py
@@ -1,3 +1,17 @@
+"""
+pytest test for function lines_from_file using file 'example_in.txt'
+in this directory.
+
+Note this is not really a UNIT test as it opens an actual file, it
+is more of an integration test.
+A unit test tests the functioning of the module in isolation from
+other modules, particularly if they involve inteaction with external
+resources such as disk files, database access or web access. This
+is normally done by mocking. 
+
+see 
+http://blog.thedigitalcatonline.com/blog/2016/03/06/python-mocks-a-gentle-introduction-part-1/
+"""
 import os
 from task_2_function_to_read_a_file_into_lines import lines_from_file
 
@@ -5,7 +19,7 @@ def example_in_dot_txt_filename():
     """
     returns the absolute path for file 'example_in.txt'
     in the same directory as this file.
-    Needed because tess need to run independently from
+    Needed because tests need to run independently from
     any directory
     """
     this_directory = os.path.dirname(os.path.abspath(__file__))
