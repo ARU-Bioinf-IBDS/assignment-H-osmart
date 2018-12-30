ddddcd9ba0648af49b316efedb92647aba966c19
Oliver Smart <oliver.s.smart@gmail.com>
2018-12-30 15:59:02 +0000

    add TODO with task

--- 1_file_io/task_3_write_a_file.py
+++ 1_file_io/task_3_write_a_file.py
@@ -3,9 +3,5 @@
 # in a FASTA format
 label='Test_fasta_99999'
 sequence='CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACT'
-with open('test_out.fasta', mode='w') as file_out:
-    #file_out.write('>' + label + '\n')
-    #file_out.write(sequence + '\n')
-    print('>' + label, file=file_out)
-    print(sequence, file=file_out)
+# TODO write out data in FASTA format to test_out.fasta
 
