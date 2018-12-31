ca09f822f9e1961198754e4c78a3925341a48a44
Oliver Smart <oliver.s.smart@gmail.com>
2018-12-31 09:29:51 +0000

    improve test with function filename_in_this_directory to allow reuse

--- 1_file_io/test_lines_from_file.py
+++ 1_file_io/test_lines_from_file.py
@@ -7,30 +7,35 @@ is more of an integration test.
 A unit test tests the functioning of the module in isolation from
 other modules, particularly if they involve inteaction with external
 resources such as disk files, database access or web access. This
-is normally done by mocking. 
+is normally done by mocking.
 
-see 
+see
 http://blog.thedigitalcatonline.com/blog/2016/03/06/python-mocks-a-gentle-introduction-part-1/
 """
 import os
 from task_2_function_to_read_a_file_into_lines import lines_from_file
 
-def example_in_dot_txt_filename():
+
+def test_read_lines_from_example_in_dot_txt():
+    example_in_dot_txt = filename_in_this_directory('example_in.txt')
+    assert lines_from_file(example_in_dot_txt) == \
+        ['this is first line from example_in.txt',
+         'second line!', '3rd and final line.']
+
+
+def filename_in_this_directory(filename):
     """
-    returns the absolute path for file 'example_in.txt'
+    returns the absolute path for file filename
     in the same directory as this file.
+
     Needed because tests need to run independently from
     any directory
+
+    Raises:
+        RuntimeError if the filename does not exist
     """
     this_directory = os.path.dirname(os.path.abspath(__file__))
-    test_filename = os.path.join(this_directory, 'example_in.txt')
+    test_filename = os.path.join(this_directory, filename)
     if not os.path.isfile(test_filename):
         raise RuntimeError('test setup failed to find file:' + test_filename)
     return test_filename
-
-def test_read_lines_from_example_in_dot_txt():
-    example_in_dot_txt = example_in_dot_txt_filename()
-    assert lines_from_file(example_in_dot_txt) == \
-        ['this is first line from example_in.txt', 
-         'second line!', '3rd and final line.']
-
