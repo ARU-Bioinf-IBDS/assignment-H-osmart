059c954351612d440701262ee82165d571ba1669
Oliver Smart <oliver.s.smart@gmail.com>
2018-12-31 09:05:26 +0000

    add test there is a docstring

--- 2_fasta_gc_content/test_percent_gc.py
+++ 2_fasta_gc_content/test_percent_gc.py
@@ -19,3 +19,8 @@ def test_percent_gc_AGCTATAG():
     # from http://rosalind.info/problems/gc/
     # For example, the GC-content of "AGCTATAG" is 37.5%.
     assert percent_gc('AGCTATAG') == pytest.approx(37.5)
+
+
+def test_percent_gc_has_docstring():
+    # just check there is one as cannot check that it is human readable
+    assert len(percent_gc.__doc__) > 0
