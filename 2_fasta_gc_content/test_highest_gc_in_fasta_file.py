fa87d4ec1dd0a5edf20742a7e40db9d5d88d646c
Oliver Smart <oliver.s.smart@gmail.com>
2019-01-01 11:27:45 +0000

    add test for actual Rosalind test sample output

--- 2_fasta_gc_content/test_highest_gc_in_fasta_file.py
+++ 2_fasta_gc_content/test_highest_gc_in_fasta_file.py
@@ -10,17 +10,18 @@ from fasta_gc_content import highest_gc_in_fasta_file
 from test_lines_from_file import filename_in_this_directory
 
 
-def test_read_from_simple_fasta_dot_txt():
+def test_from_simple_fasta_dot_txt():
     simple_fasta_dot_txt = filename_in_this_directory('simple_fasta.txt')
     max_id, max_percent = highest_gc_in_fasta_file(simple_fasta_dot_txt)
     assert max_id == 'minimal_fasta_with_single_short_sequence'
     assert max_percent == pytest.approx(50)
 
-#def test_read_from_rosalind_sample_dot_txt():
-#    rosalind_sample_dot_txt = filename_in_this_directory('rosalind_sample.txt')
-#    ids, seqs = parse_fasta_file(rosalind_sample_dot_txt)
-#    assert ids == ['Rosalind_6404', 'Rosalind_5959', 'Rosalind_0808']
-#    assert len(seqs) == 3 
+def test_from_rosalind_sample_dot_txt():
+    rosalind_sample_dot_txt = filename_in_this_directory('rosalind_sample.txt')
+    max_id, max_percent = highest_gc_in_fasta_file(rosalind_sample_dot_txt)
+    # From "Sample Output" given at http://rosalind.info/problems/gc/
+    assert max_id == 'Rosalind_0808'
+    assert max_percent == pytest.approx(60.919540)
 
 def test_function_has_docstring():
     # just check there is one as cannot check that it is human readable
