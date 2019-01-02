c2b62e0ea6cb29f461b88ca5134b6e45b79f5220
Oliver Smart <oliver.s.smart@gmail.com>
2019-01-02 14:05:56 +0000

    add stub implementations of functions to be written.
    All tests currently fail:
    > $ pytest | less:
    > ============================= test session starts ==============================
    > platform darwin -- Python 3.6.3, pytest-3.2.1, py-1.4.34, pluggy-0.4.0
    > rootdir: /Users/osmart/ARU/prac-H/2_fasta_gc_content, inifile:
    > plugins: cov-2.6.0
    > collected 20 items
    >
    > test_gc_percent.py FFFFF
    > test_gc_percentages.py FFF
    > test_highest_gc_in_fasta_file.py FFF
    > test_lines_from_file.py FFF
    > test_parse_fasta_file.py FFF
    > test_parse_fasta_lines.py FFF
    >
    > =================================== FAILURES ===================================
    > .... loads of output
    > ========================== 20 failed in 0.15 seconds ===========================

--- 2_fasta_gc_content/fasta_gc_content.py
+++ 2_fasta_gc_content/fasta_gc_content.py
@@ -5,3 +5,27 @@ entry has the highest percentage GC content.
 written to solve Rosalind Problem "Computing GC Content"
 http://rosalind.info/problems/gc/
 """
+
+
+def lines_from_file(filename):
+    pass  # TODO osmart Jan 2019: write function & get rid of this line
+
+
+def parse_fasta_from_lines(lines):
+    pass  # TODO osmart Jan 2019: write function & get rid of this line
+
+
+def parse_fasta_file(filename):
+    pass  # TODO osmart Jan 2019: write function & get rid of this line
+
+
+def gc_percent(sequence):
+    pass  # TODO osmart Jan 2019: write function & get rid of this line
+
+
+def gc_percentages(seqs):
+    pass  # TODO osmart Jan 2019: write function & get rid of this line
+
+
+def highest_gc_in_fasta_file(file_name):
+    pass  # TODO osmart Jan 2019: write function & get rid of this line
