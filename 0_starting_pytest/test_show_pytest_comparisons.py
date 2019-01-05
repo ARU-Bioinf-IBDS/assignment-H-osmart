475dde0de1c60ecc57e33c01de440ef3986669c0
Oliver Smart <oliver.s.smart@gmail.com>
2019-01-05 11:37:16 +0000

    fix up example test comparisons so all tests pass.

--- 0_starting_pytest/test_show_pytest_comparisons.py
+++ 0_starting_pytest/test_show_pytest_comparisons.py
@@ -8,23 +8,22 @@ from math import sqrt
 
 def test_show_how_pytest_compares_lists():
     abcde = 'abcde'
-    assert list('abcde') == ['a', 'b', 'c', 'd']
-    assert list('abcde') == ['a', 'c', 'b', 'd', 'e']
-
+    assert list('abcde') == ['a', 'b', 'c', 'd', 'e']
+    
 
 def test_show_how_to_compare_floats():
     # rounding errors can cause problems 
     # use pytest.approx fix
-    assert 0.1 + 0.2 == 0.3
+    assert 0.1 + 0.2 == pytest.approx(0.3)
 
 
 def test_more_on_pytest_approx():
     # square root of 2.0 is 1.414213
-    assert sqrt(2.) == pytest.approx(1.414)
+    assert sqrt(2.) == pytest.approx(1.414213)
 
 
 # to test that a particular kind of exception is raised.
 def test_sqrt_negative_number_raises_exception():
     # N.B. sqrt negative number raises ValueError rather than IOError
-    with pytest.raises(IOError):
+    with pytest.raises(ValueError):
         sqrt(-2.0)
