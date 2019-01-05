696db39b5ee4a503e8be435bcf218e4cbb3be415
Oliver Smart <oliver.s.smart@gmail.com>
2019-01-05 17:32:46 +0000

    pytests for count_vowels function
    Tests do not run because we have not yet written the function.
    
    $ pytest test_count_vowels.py
    ============================= test session starts ==============================
    platform darwin -- Python 3.6.7, pytest-3.2.1, py-1.4.34, pluggy-0.4.0
    rootdir: /Users/osmart/IBDS/assignment-H-osmart/0_starting_pytest, inifile:
    plugins: cov-2.6.0
    collected 0 items / 1 errors
    
    ==================================== ERRORS ====================================
    ____________________ ERROR collecting test_count_vowels.py _____________________
    ImportError while importing test module '/Users/osmart/IBDS/assignment-H-osmart/0_starting_pytest/test_count_vowels.py'.
    Hint: make sure your test modules/packages have valid Python names.
    Traceback:
    test_count_vowels.py:8: in <module>
        from count_vowels import count_vowels
    E   ModuleNotFoundError: No module named 'count_vowels'
    !!!!!!!!!!!!!!!!!!! Interrupted: 1 errors during collection !!!!!!!!!!!!!!!!!!!!
    =========================== 1 error in 0.16 seconds ============================

--- 0_starting_pytest/test_count_vowels.py
+++ 0_starting_pytest/test_count_vowels.py
@@ -5,3 +5,25 @@ Instructions:
     for lower and UPPER case strings. All strings will be in
     English.
 """
+from count_vowels import count_vowels
+
+def test_count_vowels_e():
+    assert count_vowels('e') == 1
+    
+
+def test_count_vowels_all_vowels():
+    assert count_vowels('aeiou') == 5
+    
+
+def test_count_vowels_uppercase_IOU():
+    assert count_vowels('IOU') == 3
+    
+def test_count_vowels_non_vowels():
+    assert count_vowels('sthvxyz., & ') == 0
+  
+def test_count_vowels_Anglia_Ruskin():
+    assert count_vowels('Anglia Ruskin University') == 9
+  
+def test_count_vowels_empty():
+    assert count_vowels('') == 0
+
