a301fd3b7702fda38e619efec75f7078816d1b0f
Oliver Smart <oliver.s.smart@gmail.com>
2019-01-05 17:44:14 +0000

    count_vowels function, all pytests pass

--- 0_starting_pytest/count_vowels.py
+++ 0_starting_pytest/count_vowels.py
@@ -1,6 +1,7 @@
 def count_vowels(str):
     """ returns the number of vowels (aeiou) in a string str. """
     vowels = 'aeiou'
+    vowels += vowels.upper()
     answer = 0
     for v in vowels:
         answer += str.count(v)
