54d1fff984cedf3ff7545136cf55706be9c4daec
Oliver Smart <oliver.s.smart@gmail.com>
2019-01-08 14:46:36 +0000

    reads example_in.txt successfully.
    example output:
    
    $ python mini_1a_read_a_file.py
    this is first line from example_in.txt
    second line!
    3rd and final line.

--- 1_file_io/mini_1a_read_a_file.py
+++ 1_file_io/mini_1a_read_a_file.py
@@ -1,3 +1,7 @@
 # instructions: write python to read the file example_in.txt
 # and print out its contents to the screen.
 # Hint: you will find what you need on the reference card
+in_file = open('example_in.txt')
+contents = in_file.read()
+in_file.close()
+print(contents, end='')
