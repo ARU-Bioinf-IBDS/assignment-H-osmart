344a364a3ab125929ca6de23df9141391b137f63
Oliver Smart <oliver.s.smart@gmail.com>
2019-01-08 14:50:27 +0000

    Alternative open file using with.
    Same output produced:
    
    $ python mini_1a_read_a_file.py
    this is first line from example_in.txt
    second line!
    3rd and final line.

--- 1_file_io/mini_1a_read_a_file.py
+++ 1_file_io/mini_1a_read_a_file.py
@@ -1,7 +1,6 @@
 # instructions: write python to read the file example_in.txt
 # and print out its contents to the screen.
 # Hint: you will find what you need on the reference card
-in_file = open('example_in.txt')
-contents = in_file.read()
-in_file.close()
+with open('example_in.txt') as in_file:
+    contents = in_file.read()
 print(contents, end='')
