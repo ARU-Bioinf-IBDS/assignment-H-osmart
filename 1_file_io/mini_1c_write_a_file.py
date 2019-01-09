b5ee1d09caaa8dec55ce534c92832c1d8833e196
Oliver Smart <oliver.s.smart@gmail.com>
2019-01-09 13:47:14 +0000

    change to 'with open() as ...'.
    Manual test that it works as it should:
    
    $ python mini_1c_write_a_file.py
    $ cat test_out.fasta
    >Test_fasta_99999
    CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACT

--- 1_file_io/mini_1c_write_a_file.py
+++ 1_file_io/mini_1c_write_a_file.py
@@ -6,7 +6,6 @@
 # Hint: look at the fileio_in_python.pdf reference card! 
 label='Test_fasta_99999'
 sequence='CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACT'
-file_out = open('test_out.fasta', 'w')
-file_out.write('>' + label + '\n')
-file_out.write(sequence + '\n')
-file_out.close()
+with open('test_out.fasta', 'w') as file_out:
+    file_out.write('>' + label + '\n')
+    file_out.write(sequence + '\n')
